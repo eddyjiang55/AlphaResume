05fc377a6659d1ecffdc5157ca5c9e1990a07326
syf0417 <1922038418@qq.com>
2024-03-21 20:15:03 -0500

    login update

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -9,19 +9,25 @@ const LoginPage = () => {
             <div className="login-form">
             <div className='backward'>
                     <img src='/img/back.svg'width={40} height={40} ></img>
-                    <a href='/choose-resumeserve-type'>返回</a>
+                    <a href='/'>返回</a>
             </div>
                 <div className="text-title">登陆</div>
                 <input type="text" placeholder="手机号码" />
                 <input type="password" placeholder="验证码" />
                 <button className='action-button'>登录</button>
                 <p className="hint">未注册的手机号验证后自动创建账号</p>
+                <div className='third-party'>
+                  <span>第三方账号快速登录</span>
+                  <img src='/img/wechat.svg' width={50}></img>
+                </div>
             </div>
             <div className="info-section">
                 <img src='/img/logo_y.jpg' width={190}></img>
-                <p className='para1'>从简历到面试，每一步都需精心准备，确保领先在起跑线！简历和面试是求职关键，充足准备才能脱颖而出。别小看每一步，从展现经历到面试应对，都至关重要。</p>
-                <p className='para2'>放心，我们全程陪伴，助你求职之路更顺畅简单！</p>
-                <img></img>
+                <div className='text-container'>
+                  <div className='para'>从简历到面试，每一步都需精心准备，确保领先在起跑线！简历和面试是求职关键，充足准备才能脱颖而出。别小看每一步，从展现经历到面试应对，都至关重要。</div>
+                  <div className='para'>放心，我们全程陪伴，助你求职之路更顺畅简单！</div>
+                </div>
+                <img src='/img/background.jpg'></img>
             </div>
         </div>
       <style jsx>{`
@@ -30,6 +36,26 @@ const LoginPage = () => {
           justify-content: space-between;
           min-height:100vh;
           min-width:100vw;
+        }
+        .third-party{
+          display: flex;
+          justify-content: center;
+          flex-direction: column; 
+          align-items: center;
+          padding:10px;
+          gap:20px;
+        }
+        .text-container{
+          display: flex;
+          justify-content: center;
+          flex-direction: column; 
+          text-align: center;
+          padding:100px;
+          gap:30px;
+          font-size:24px;
+        }
+        .para{
+          color:#1D80A7;
         }
         .backward {
             display: flex;
@@ -73,6 +99,8 @@ const LoginPage = () => {
         .hint {
           font-size: 12px;
           color: white;
+          border-bottom:1px solid grey;
+          padding-bottom:30px;
         }
         .info-section {
           display: flex;
@@ -97,8 +125,9 @@ const LoginPage = () => {
             border-radius: 10px;
             padding-left: 50px;
             padding-right: 50px;
-            margin-top:50px;
+            margin-top:100px;
             margin-bottom:20px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
         }
         .action-button:hover {
             background-color: #EDF8FD;
