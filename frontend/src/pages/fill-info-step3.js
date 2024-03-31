4c2f2ecdb808b851412e0173507cdd2463753e8d
syf0417 <1922038418@qq.com>
2024-03-30 21:31:32 -0500

    fix_error

--- frontend/src/pages/fill-info-step3.js
+++ frontend/src/pages/fill-info-step3.js
@@ -62,7 +62,7 @@ const HomePage = () => {
 
               <div className="form-buttons">
                 <button className='form-b' type="submit">保存</button>
-                <button className='form-b' type="button"><a href='/fill-info-step2'>下一步</a></button>
+                <button className='form-b' type="button"><a href='/fill-info-step4'>下一步</a></button>
               </div>
             </form>
           </div>
