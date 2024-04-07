50905c2f27eeea1a8e3cb9429e9a9b0f7de868da
ruiqing yu <leoyuruiqing@gmail.com>
2024-04-07 13:25:40 -0500

    create branch

---
+++ frontend/src/pages/ai-assistant-chat.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const AIChat = () => {
+  return (
+    <div className="bg-[#1D80A7] w-full h-screen">
+      <div className='flex flex-col justify-center items-stretch'>
+
+      </div>
+
+    </div>
+  )
+}
