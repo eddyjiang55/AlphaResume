3c4b5755059f4bc0d5a4cb713b20a4fdecaba742
Hickey17 <newzhongwen23@gmail.com>
2024-04-12 09:50:44 -0700

    sim

--- backend/services/smsService.js
+++ backend/services/smsService.js
@@ -4,8 +4,8 @@ const SMSClient = require('@alicloud/sms-sdk');
 
 // 初始化SMSClient
 const smsClient = new SMSClient({
-  accessKeyId: '',
-  secretAccessKey: '',
+  accessKeyId: 'LTAI5tELxePPrwBRmfMmhdkc',
+  secretAccessKey: '6WXwK5cjZSak1DxQ3F2pFhXZ7BkYbg',
 });
 
 // 发送短信的函数
