03e8611af90bd66e168e9e7c229b170b778682f8
Hickey17 <newzhongwen23@gmail.com>
2024-04-19 23:33:50 -0700

    try to update zhongwen branch to fit main

--- backend/mongodb/dbconfig.js
+++ backend/mongodb/dbconfig.js
@@ -1,13 +1,23 @@
-const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const url = 'mongodb://localhost:27017'; // MongoDB URL
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.MONGODB_URL;
 const dbName = 'airesumedb'; // Database Name
 
-const client = new MongoClient(url);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 async function connect() {
   try {
     await client.connect();
+    await client.db("admin").command({ ping: 1 });
     console.log("Connected successfully to MongoDB");
     const db = client.db(dbName);
     return db;
