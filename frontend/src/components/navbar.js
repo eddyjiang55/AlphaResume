f9a2079018eb8cd97f8739ed012e1ac6e9bd9a48
syf0417 <1922038418@qq.com>
2024-05-15 09:47:58 -0500

    personal_info page done

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -75,7 +75,9 @@ const Navbar = () => {
                     {phoneNumber ? (
                         <>
                             <div className="user-avatar">
-                                <Image src="/img/user-avatar.jpg" alt="User Avatar" width={40} height={40} className="rounded-full" />
+                                <Link href='/personal_info'>
+                                    <Image src="/img/user-avatar.jpg" alt="User Avatar" width={40} height={40} className="rounded-full" />
+                                </Link>
                             </div>
                             <button className="action-button px-7 py-2 border border-[#1D80A7] bg-white text-[#1D80A7] cursor-pointer rounded-md hover:bg-[#1D80A7] hover:text-white" onClick={handleLogout}>登出</button>
                         </>
