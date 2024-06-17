ece0f366618bd4cee50abd3a7244f3ed18418fb7
syf0417 <1922038418@qq.com>
2024-06-17 14:40:39 -0500

    下拉菜单

--- frontend/src/components/authPart.js
+++ frontend/src/components/authPart.js
@@ -16,10 +16,17 @@ const AuthPart = () => {
     <div className="actions flex gap-2.5 mr-18">
       {user.phoneNumber ? (
         <>
-          <Link href='/personal_info'>
-            <Image src="/img/user-avatar.jpg" alt="User Avatar" width={40} height={40} className="rounded-full" />
-          </Link>
-          <button className="action-button px-7 py-2 border border-[#1D80A7] bg-white text-[#1D80A7] cursor-pointer rounded-md hover:bg-[#1D80A7] hover:text-white" onClick={handleLogout}>登出</button>
+          <div className="relative">
+            <Link href='/personal_info'>
+              <Image src="/img/user-avatar.jpg" alt="User Avatar" width={40} height={40} className="rounded-full cursor-pointer" />
+            </Link>
+            <div className="dropdown absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg">
+              <ul>
+                <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer" onClick={handleSettings}>设置</li>
+                <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer" onClick={handleLogout}>登出</li>
+              </ul>
+            </div>
+          </div>
         </>
       ) : (
         <>
