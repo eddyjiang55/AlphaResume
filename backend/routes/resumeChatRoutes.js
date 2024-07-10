19d603eefff71154638ca01e84278243474c49c6
Hickey17 <newzhongwen23@gmail.com>
2024-07-10 12:00:57 -0700

    修改了imp除了generate之外的接口

--- backend/routes/resumeChatRoutes.js
+++ backend/routes/resumeChatRoutes.js
@@ -13,7 +13,7 @@ function getFormattedDate() {
 }
 
 // 获取 Python 脚本的绝对路径
-const pythonScriptPath = path.resolve(__dirname, '../pyScripts/AI_asking_new.py');
+const pythonScriptPath = path.resolve(__dirname, '../pyScripts/AI_asking_method1.py');
 
 router.post('/resume-chat', async (req, res) => {
     try {
